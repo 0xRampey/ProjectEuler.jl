8decb17bcd4d7c5f221943ab81a2c3cbea26726d
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2021-07-19 14:22:39 +0530

    Add Sieve of Eratosthenes

--- utils.jl
+++ utils.jl
@@ -1,3 +1,5 @@
+# using BenchmarkTools
+
 function is_prime(num)
     # Every non-prime number will have atleast one factor <= its square root.
     # Just need to find that
@@ -9,5 +11,23 @@ function is_prime(num)
     return true
 end
 
+function get_prime_sieve_to_n(n)
+    # Assume all to be prime and unmarked
+    # false means marked or not prime
+    sieve = trues(n)
+    sieve[1] = false 
+    for idx in 2:isqrt(n)
+        # If index is true, it is prime
+        if sieve[idx]         
+            # Mark its multiples to be not prime
+            for i in 2:floor(Int64, (n / idx))
+                sieve[i * idx] = false
+            end
+        end
+    end
+    return sieve
+end
+
+# @time println(findall(get_prime_factor_sieve_to_n(100)))
 # Module and import system in Julia has quite the learning curve.
 # Definitely not user friendly.
